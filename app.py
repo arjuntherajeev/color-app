acd2360a65896a92ed6c87a9cb95c99e7b6fa673
Arjun <arjun.nedungadi1@ibm.com>
2023-10-17 16:48:55 +0400

    fixed

--- app.py
+++ app.py
@@ -17,25 +17,14 @@ color_codes = {
     "purple": "#7d3c98"
 }
 
-#color = os.environ.get('APP_COLOR') or random.choice(["red","green"])
-#color = os.environ.get('APP_COLOR') or random.choice(["white"])
-#color = os.environ.get('APP_COLOR') or random.choice(["pink","blue","yellow"])
-
 @app.route("/")
 def main():
-    #return 'Hello'
-    #print(color)
+    color = random.choice(["pink","blue","yellow"])
     return render_template('hello.html', name=socket.gethostname(), color=color_codes[color])
 
-@app.route('/color/<new_color>')
-def new_color(new_color):
-    return render_template('hello.html', name=socket.gethostname(), color=color_codes[new_color])
-
-@app.route('/read_file')
-def read_file():
-    f = open("/data/testfile.txt")
-    contents = f.read()
-    return render_template('hello.html', name=socket.gethostname(), contents=contents, color=color_codes[color])
+@app.route("/test")
+def test():
+    return "TEST!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="8080")
